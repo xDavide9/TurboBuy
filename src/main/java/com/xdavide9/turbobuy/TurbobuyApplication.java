7bb988c13d922da84ad99a6082d1929f03ad91e4
xdavide9 <biancodavidetech@gmail.com>
2022-09-20 22:03:25 +0200

    bootstrap application

---
+++ src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
@@ -0,0 +1,13 @@
+package com.xdavide9.turbobuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbobuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbobuyApplication.class, args);
+	}
+
+}
