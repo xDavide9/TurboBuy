ee850973328e1690d789d5767b4a9139706052f8
xdavide9 <biancodavidetech@gmail.com>
2022-09-27 22:04:58 +0200

    add some todos in main class

--- src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
+++ src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TurbobuyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TurbobuyApplication.class, args);
-	}
+    // todo implement csrf
+    // todo implement custom exception handling and passing to frontend
+    // todo implement /register view with a form that perform a post request to its same url
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbobuyApplication.class, args);
+    }
 
 }
