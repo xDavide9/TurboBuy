a167806b39b2a848c7ba246d7220b91106adaf4a
xdavide9 <biancodavidetech@gmail.com>
2022-09-30 19:30:42 +0200

    create register form; change accepted data and remove @RequestBody in RequestController

--- src/main/java/com/xdavide9/turbobuy/registration/RegistrationController.java
+++ src/main/java/com/xdavide9/turbobuy/registration/RegistrationController.java
@@ -3,7 +3,6 @@ package com.xdavide9.turbobuy.registration;
 import com.xdavide9.turbobuy.user.AppUser;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +13,8 @@ public class RegistrationController {
 
     private final RegistrationService registrationService;
 
-    @PostMapping(consumes = {"application/json"})
-    public void register(@RequestBody AppUser user) {
+    @PostMapping(consumes = "application/x-www-form-urlencoded")
+    public void register(AppUser user) {
         registrationService.register(user);
     }
 }
