27859224c455d38dd48a10d930fa5e8e88c51f52
xdavide9 <biancodavidetech@gmail.com>
2022-10-06 20:25:57 +0200

    rename package to template

--- src/main/java/com/xdavide9/turbobuy/template/TemplateController.java
+++ src/main/java/com/xdavide9/turbobuy/template/TemplateController.java
@@ -1,4 +1,4 @@
-package com.xdavide9.turbobuy.controller;
+package com.xdavide9.turbobuy.template;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
