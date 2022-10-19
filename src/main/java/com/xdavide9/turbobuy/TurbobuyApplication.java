b1ac17880fcd2381945cdc1cb5d9e32616289fb0
xdavide9 <biancodavidetech@gmail.com>
2022-10-19 22:09:54 +0200

    update todos

--- src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
+++ src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TurbobuyApplication {
 
     // todo implement csrf
-    // todo start creating frontend with bootstrap and thymeleaf
     public static void main(String[] args) {
         SpringApplication.run(TurbobuyApplication.class, args);
     }
