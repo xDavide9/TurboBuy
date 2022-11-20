9f85df3eba1b2f3a508a2fe69a32814f48546715
xdavide9 <biancodavidetech@gmail.com>
2022-11-20 19:25:02 +0100

    refactor account section

--- src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
+++ src/main/java/com/xdavide9/turbobuy/TurbobuyApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TurbobuyApplication {
 
-    // todo 1) create footer
     // todo 2) create contact section
     // todo 3) refactor forms to use thymeleaf features
     // todo 4) add image support to the application
